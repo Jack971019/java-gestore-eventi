2849c4ed0b1a1e8c5b99c860fdbe869bad775251
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-05 13:04:06 +0200

    added cancel method in class main

--- src/main/java/org/lessons/Eventi/Main.java
+++ src/main/java/org/lessons/Eventi/Main.java
@@ -55,6 +55,27 @@ public class Main {
         System.out.println("posti prenotati" + evento.getNumeroPostiPrenotati());
         System.out.println("posti ancora disponibili: " + (evento.getNumeroPostiTotale()- evento.getNumeroPostiPrenotati()));
 
+        boolean disdirePrenotazione = false;
+        while(!disdirePrenotazione) {
+            System.out.println("vuoi disdire una prenotazione? Y/N");
 
+            String userChoice = scan.nextLine();
+            if (userChoice.equalsIgnoreCase("Y")) {
+                try {
+                    System.out.println("quanti posti vuoi disdire?");
+                    int userPrenotazione = Integer.parseInt(scan.nextLine());
+                    evento.disdiciPrenotazioni(userPrenotazione);
+                } catch (RuntimeException e) {
+                    System.out.println("Inserire dei dati validi");
+                }
+
+            } else if (userChoice.equalsIgnoreCase("N")) {
+                System.out.println("Grazie Arrivederci");
+                disdirePrenotazione = true;
+            }
+        }
+
+        System.out.println("posti prenotati" + evento.getNumeroPostiPrenotati());
+        System.out.println("posti ancora disponibili" + (evento.getNumeroPostiTotale() - evento.getNumeroPostiPrenotati()));
     }
 }
